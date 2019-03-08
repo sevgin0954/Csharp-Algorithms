3baf283fb6c9535b47fa07b567c9314136580537
sevgin0954 <skateboardista_0954@abv.bg>
2019-03-08 21:17:00 +0200

    added new solutions

---
+++ ReverseInt32/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Reverse(n));
+    }
+
+    public static int Reverse(int x)
+    {
+        char[] arr = x.ToString().ToCharArray();
+
+        int startIndex = 0;
+        int endIndex = arr.Length - 1;
+
+        if (arr[0] == '-')
+        {
+            startIndex = 1;
+        }
+
+        while (startIndex < endIndex)
+        {
+            char temp = arr[startIndex];
+            arr[startIndex] = arr[endIndex];
+            arr[endIndex] = temp;
+
+            startIndex++;
+            endIndex--;
+        }
+
+        string reversedNumAsString = string.Join("", arr);
+
+        int result = int.Parse(reversedNumAsString);
+
+        return result;
+    }
+}
