f0aa3c626c6bee739289b1fc6609858a6026004b
sevgin0954 <skateboardista_0954@abv.bg>
2019-03-12 00:22:13 +0200

    added new solutions

---
+++ Pow(x, n)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pow_x__n_
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(MyPow(34.00515, -3));
+        }
+
+        public static double MyPow(double x, int n)
+        {
+            return PowerRecursion(x, n, new Dictionary<int, double>());
+        }
+
+        public static double PowerRecursion(double x, int n, Dictionary<int, double> dictionary)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            if (n == 1 || n == -1)
+            {
+                return x;
+            }
+            if (dictionary.ContainsKey(n) == true)
+            {
+                return dictionary[n];
+            }
+
+            double result = 0;
+
+            if (n % 2 == 0)
+            {
+                result = PowerRecursion(x, n / 2, dictionary) * PowerRecursion(x, n / 2, dictionary);
+            }
+            else
+            {
+                result = x * PowerRecursion(x, n / 2, dictionary) * PowerRecursion(x, n / 2, dictionary);
+            }
+
+            if (n < 0)
+            {
+                result = 1 / result;
+            }
+
+            dictionary[n] = result;
+
+            return result;
+        }
+    }
+}
---
+++ RotateMatrix/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace RotateMatrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] matrix =
+            {
+                { 1,2,3 },
+                { 4,5,6 },
+                { 7,8,9 }
+            };
+
+            RotateMatrix(matrix);
+
+            PrintMatrix(matrix);
+        }
+
+        static void RotateMatrix(int[,] matrix)
+        {
+            int n = matrix.GetLength(0);
+
+            for (int rowCol = 0; rowCol < n / 2; rowCol++)
+            {
+                for (int col = rowCol; col < n - 1 - rowCol; col++)
+                {
+                    int[] upperRowIndex = new int[] { rowCol, col };
+                    int[] rightColIndex = new int[] { col, n - rowCol - 1 };
+                    int[] bottomRowIndex = new int[] { n - rowCol - 1, n - col - 1 };
+                    int[] leftColIndex = new int[] { n - col - 1, rowCol };
+
+                    int upperLeft = matrix[upperRowIndex[0], upperRowIndex[1]];
+
+                    // Move from bottom left to up
+                    matrix[upperRowIndex[0], upperRowIndex[1]] = matrix[leftColIndex[0], leftColIndex[1]];
+
+                    // Move from bottom right to left
+                    matrix[leftColIndex[0], leftColIndex[1]] = matrix[bottomRowIndex[0], bottomRowIndex[1]];
+
+                    // Move from upper right to bottom
+                    matrix[bottomRowIndex[0], bottomRowIndex[1]] = matrix[rightColIndex[0], rightColIndex[1]];
+
+                    // Move from upper left to right
+                    matrix[rightColIndex[0], rightColIndex[1]] = upperLeft;
+                }
+            }
+        }
+
+        private static void PrintMatrix(int[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
